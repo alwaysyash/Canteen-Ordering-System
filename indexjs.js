818e571919ec4b8e47431dd3d1123432b3767072
Yash <yash.chopade1209@gmail.com>
2023-04-28 01:21:19 +0530

    buttonn event handling and reload handling

--- indexjs.js
+++ indexjs.js
@@ -1,55 +1,67 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.20.0/firebase-app.js";
+import {
+    initializeApp
+} from "https://www.gstatic.com/firebasejs/9.20.0/firebase-app.js";
 
-      // If you enabled Analytics in your project, add the Firebase SDK for Google Analytics
-      import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.20.0/firebase-analytics.js";
+// If you enabled Analytics in your project, add the Firebase SDK for Google Analytics
+import {
+    getAnalytics
+} from "https://www.gstatic.com/firebasejs/9.20.0/firebase-analytics.js";
 
-      // Add Firebase products that you want to use
-      import { getAuth } from "https://www.gstatic.com/firebasejs/9.20.0/firebase-auth.js";
+// Add Firebase products that you want to use
+import {
+    getAuth
+} from "https://www.gstatic.com/firebasejs/9.20.0/firebase-auth.js";
 
-      import {
-        getFirestore,
-        collection,
-        getDocs,
-      } from "https://www.gstatic.com/firebasejs/9.20.0/firebase-firestore.js";
-      const firebaseConfig = {
-        apiKey: "AIzaSyD5TKCHJrdQan6mrfk4NmuaLYhzFx_G5AI",
-        authDomain: "vihwafoodie.firebaseapp.com",
-        projectId: "vihwafoodie",
-        storageBucket: "vihwafoodie.appspot.com",
-        messagingSenderId: "811791488279",
-        appId: "1:811791488279:web:248ed320159d0debd49352",
-        measurementId: "G-LMSRJT4128",
-      };
+import {
+    getFirestore,
+    collection,
+    getDocs,
+    updateDoc,
+    doc
+} from "https://www.gstatic.com/firebasejs/9.20.0/firebase-firestore.js";
+const firebaseConfig = {
+    apiKey: "AIzaSyD5TKCHJrdQan6mrfk4NmuaLYhzFx_G5AI",
+    authDomain: "vihwafoodie.firebaseapp.com",
+    projectId: "vihwafoodie",
+    storageBucket: "vihwafoodie.appspot.com",
+    messagingSenderId: "811791488279",
+    appId: "1:811791488279:web:248ed320159d0debd49352",
+    measurementId: "G-LMSRJT4128",
+};
 
-      initializeApp(firebaseConfig);
+initializeApp(firebaseConfig);
 
-      const db = getFirestore(); //database connection
+const db = getFirestore(); //database connection
 
-      const collRef = collection(db, "orders"); //reference
-      let orders = [];
-      //get data
-      getDocs(collRef)
-        .then((snapshot) => {
-          snapshot.docs.forEach((doc) => {
+const collRef = collection(db, "orders"); //reference
+let orders = [];
+let ids = [];
+//get data
+getDocs(collRef)
+    .then((snapshot) => {
+        snapshot.docs.forEach((doc) => {
             orders.push(doc.data());
-          });
-          // console.log(orders)
-          let placed_orders = [];
-          orders.forEach((odr) => {
-            console.log(odr);
-            if (odr.status === "placed") {
-              placed_orders.push(odr);
-            }
-          });
-          console.log(placed_orders);
+            ids.push(doc.id);
+        });
+        //   console.log(ids)
+        let all_orders = [];
+        let i = 0;
+        orders.forEach((odr) => {
+            // console.log(odr);
+            odr.id = ids[i]
+            i++
+            all_orders.push(odr);
 
-          // Dynamic card code
+        });
+        //   console.log(all_orders);
 
-          const upcomingContainer = document.getElementById("upcoming");
-          const progressContainer = document.getElementById("progress");
-          const readyContainer = document.getElementById("ready");
-          let number = 1;
-          orders.forEach((order) => {
+        // Dynamic card code
+
+        const upcomingContainer = document.getElementById("upcoming");
+        const progressContainer = document.getElementById("progress");
+        const readyContainer = document.getElementById("ready");
+        let number = 1;
+        orders.forEach((order) => {
             // console.log(order.status)
 
             // Create card element
@@ -62,10 +74,10 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/9.20.0/firebas
             // Create card header
             const cardHeader = document.createElement("div");
             cardHeader.classList.add(
-              "card-header",
-              "bg-transparent",
-              "border-dark",
-              "my-1"
+                "card-header",
+                "bg-transparent",
+                "border-dark",
+                "my-1"
             );
 
             const orderNumberHeading = document.createElement("h5");
@@ -85,28 +97,28 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/9.20.0/firebas
             const headerRow = document.createElement("tr");
             const headerColumns = ["Item #", "Name", "Quantity"];
             headerColumns.forEach((columnTitle) => {
-              const column = document.createElement("th");
-              column.textContent = columnTitle;
-              headerRow.appendChild(column);
+                const column = document.createElement("th");
+                column.textContent = columnTitle;
+                headerRow.appendChild(column);
             });
             thead.appendChild(headerRow);
             table.appendChild(thead);
 
             const tbody = document.createElement("tbody");
             order.food_items.forEach((item, index) => {
-              console.log(item);
-              const row = document.createElement("tr");
-              const itemNumber = document.createElement("th");
-              itemNumber.setAttribute("scope", "row");
-              itemNumber.textContent = index + 1;
-              const itemName = document.createElement("td");
-              itemName.textContent = item.menu_item.name;
-              const itemQuantity = document.createElement("td");
-              itemQuantity.textContent = item.quantity;
-              row.appendChild(itemNumber);
-              row.appendChild(itemName);
-              row.appendChild(itemQuantity);
-              tbody.appendChild(row);
+                console.log(item);
+                const row = document.createElement("tr");
+                const itemNumber = document.createElement("th");
+                itemNumber.setAttribute("scope", "row");
+                itemNumber.textContent = index + 1;
+                const itemName = document.createElement("td");
+                itemName.textContent = item.menu_item.name;
+                const itemQuantity = document.createElement("td");
+                itemQuantity.textContent = item.quantity;
+                row.appendChild(itemNumber);
+                row.appendChild(itemName);
+                row.appendChild(itemQuantity);
+                tbody.appendChild(row);
             });
             table.appendChild(tbody);
             cardBody.appendChild(table);
@@ -115,62 +127,108 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/9.20.0/firebas
             // Create card footer
             const cardFooter = document.createElement("div");
             cardFooter.classList.add(
-              "card-footer",
-              "bg-transparent",
-              "border-dark",
-              "d-flex",
-              "justify-content-center"
+                "card-footer",
+                "bg-transparent",
+                "border-dark",
+                "d-flex",
+                "justify-content-center"
             );
 
             // ordersContainer.appendChild(card);
             if (order.status == "placed") {
-              const markInProgressButton = document.createElement("button");
-              markInProgressButton.classList.add("btn1", "mt-1", "mb-1");
-              markInProgressButton.textContent = "Mark In-progress";
-              markInProgressButton.addEventListener("click", () => {
-                // Handle button click here
-              });
-              cardFooter.appendChild(markInProgressButton);
-              card.appendChild(cardFooter);
-              upcomingContainer.appendChild(card);
+                const markInProgressButton = document.createElement("button");
+                markInProgressButton.classList.add("btn1", "mt-1", "mb-1");
+                markInProgressButton.textContent = "Mark In-progress";
+                markInProgressButton.addEventListener("click", () => {
+                 
+                    const orderId = order.id;
+                    const odrRef = doc(db, "orders", orderId);
+                    updateDoc(odrRef, {
+                        status: "progress"
+                    })
+                    .then(() => {
+                        console.log("Order status updated successfully!");
+                        location.reload();
+                    })
+                    .catch((error) => {
+                        console.error("Error updating order status:", error);
+                    });
+
+                });
+                cardFooter.appendChild(markInProgressButton);
+                card.appendChild(cardFooter);
+                upcomingContainer.appendChild(card);
             }
 
             if (order.status == "progress") {
-              const markReadyButton = document.createElement("button");
-              markReadyButton.classList.add("btn1", "mt-1", "mb-1");
-              markReadyButton.textContent = "Mark Ready";
-              markReadyButton.addEventListener("click", () => {
-                // Handle button click here
-              });
-              cardFooter.appendChild(markReadyButton);
-              card.appendChild(cardFooter);
+                const markReadyButton = document.createElement("button");
+                markReadyButton.classList.add("btn1", "mt-1", "mb-1");
+                markReadyButton.textContent = "Mark Ready";
+                markReadyButton.addEventListener("click", () => {
+                    const orderId = order.id;
+                    const odrRef = doc(db, "orders", orderId);
+                    updateDoc(odrRef, {
+                        status: "ready"
+                    })
+                    .then(() => {
+                        console.log("Order status updated successfully!");
+                        location.reload();
+                    })
+                    .catch((error) => {
+                        console.error("Error updating order status:", error);
+                    });
+                });
+                cardFooter.appendChild(markReadyButton);
+                card.appendChild(cardFooter);
 
-              progressContainer.appendChild(card);
+                progressContainer.appendChild(card);
             }
             if (order.status == "ready") {
-              const deliveredButton = document.createElement("button");
-              deliveredButton.classList.add("btn1", "mt-1", "mb-1","mx-3");
-              deliveredButton.textContent = "Delivered";
-              deliveredButton.addEventListener("click", () => {
-                // Handle button click here
-              });
-              cardFooter.appendChild(deliveredButton);
+                const deliveredButton = document.createElement("button");
+                deliveredButton.classList.add("btn1", "mt-1", "mb-1", "mx-3");
+                deliveredButton.textContent = "Delivered";
+                deliveredButton.addEventListener("click", () => {
+                    const orderId = order.id;
+                    const odrRef = doc(db, "orders", orderId);
+                    updateDoc(odrRef, {
+                        status: "delivered"
+                    })
+                    .then(() => {
+                        console.log("Order status updated successfully!");
+                        location.reload();
+                    })
+                    .catch((error) => {
+                        console.error("Error updating order status:", error);
+                    });
+                });
+                cardFooter.appendChild(deliveredButton);
 
-              const cancelButton = document.createElement("button");
-              cancelButton.classList.add("btn2", "mt-1", "mb-1","mx-3");
-              cancelButton.textContent = "Cancel";
-              cancelButton.addEventListener("click", () => {
-                // Handle button click here
-              });
-              cardFooter.appendChild(cancelButton);
+                const cancelButton = document.createElement("button");
+                cancelButton.classList.add("btn2", "mt-1", "mb-1", "mx-3");
+                cancelButton.textContent = "Cancel";
+                cancelButton.addEventListener("click", () => {
+                    const orderId = order.id;
+                    const odrRef = doc(db, "orders", orderId);
+                    updateDoc(odrRef, {
+                        status: "cancelled"
+                    })
+                    .then(() => {
+                        console.log("Order status updated successfully!");
+                        location.reload();
+                    })
+                    .catch((error) => {
+                        console.error("Error updating order status:", error);
+                    });
+                });
+                cardFooter.appendChild(cancelButton);
 
-              card.appendChild(cardFooter);
+                card.appendChild(cardFooter);
 
-              readyContainer.appendChild(card);
+                readyContainer.appendChild(card);
             }
-          });
-        })
-
-        .catch((err) => {
-          console.log(err.message);
         });
+    })
+
+    .catch((err) => {
+        console.log(err.message);
+    });
